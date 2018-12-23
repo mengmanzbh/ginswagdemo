93790580ad3f67f3b40fc154729f818bc2b327df
mengmanzbh <1361006944@qq.com>
2018-12-23 21:15:15 +0800

    OK

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/swag/example/celler/controller"
+	_ "github.com/swaggo/swag/example/celler/docs"
+	"github.com/swaggo/swag/example/celler/httputil"
+
+	ginSwagger "github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/gin-swagger/swaggerFiles"
+)
+func main() {
+	r := gin.Default()
+
+	c := controller.NewController()
+
+	v1 := r.Group("/api/v1")
+	{
+
+		bottles := v1.Group("/bottles")
+		{
+			bottles.GET(":id", c.ShowBottle)
+			bottles.GET("", c.ListBottles)
+		}
+		
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(":8080")
+}
+
+func auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if len(c.GetHeader("Authorization")) == 0 {
+			httputil.NewError(c, http.StatusUnauthorized, errors.New("Authorization is required Header"))
+			c.Abort()
+		}
+		c.Next()
+	}
+}
