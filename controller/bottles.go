ecab7313def54e46ec2a848203c162c0c0541e3f
mengmanzbh <1361006944@qq.com>
2019-01-02 15:39:48 +0800

    Update bottles.go

--- controller/bottles.go
+++ controller/bottles.go
@@ -42,26 +42,6 @@ func (c *Controller) ShowBottle(ctx *gin.Context) {
 	
 }
 
-// ListBottles godoc
-// @Summary List bottles
-// @Description get bottles
-// @Tags bottles
-// @Accept  json
-// @Produce  json
-// @Success 200 {array} model.Bottle
-// @Failure 400 {object} httputil.HTTPError
-// @Failure 404 {object} httputil.HTTPError
-// @Failure 500 {object} httputil.HTTPError
-// @Router /bottles [get]
-func (c *Controller) ListBottles(ctx *gin.Context) {
-	bottles, err := model.BottlesAll()
-	if err != nil {
-		httputil.NewError(ctx, http.StatusNotFound, err)
-		return
-	}
-	ctx.JSON(http.StatusOK, bottles)
-}
-
 //发送get请求
 func Get(apiURL string,params url.Values)(rs[]byte ,err error){
 	var Url *url.URL
